c00e8330c6b20c92ee91576f926bd417f2b1657e
Felix Schmölz <felix.schmoelz@htlwy.at>
2026-09-24 08:56:37 +0200

    changed premake

--- premake5.lua
+++ premake5.lua
@@ -1,28 +1,28 @@
-project "Glad"
+project "glad"
     kind "StaticLib"
     language "C"
-    staticruntime "on"
-    
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files {
+    targetdir ("../../bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("../../bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
         "include/glad/**.h",
         "include/KHR/**.h",
         "src/**.c"
     }
 
-    includedirs {
+    includedirs
+    {
         "include"
     }
-    
-    filter "system:windows"
-        systemversion "latest"
 
     filter "configurations:Debug"
         runtime "Debug"
-        symbols "on"
+        symbols "On"
 
     filter "configurations:Release"
         runtime "Release"
-        optimize "on"
+        optimize "On"
+
+    filter {}
